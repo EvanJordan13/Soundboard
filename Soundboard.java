7ee77e5909885fba5c7e6715c6bdba5b470b0872
EvanJ <ejordan1305@gmail.com>
2020-01-08 22:41:44 -0600

    A few tweaks to try and add an action listner. (REDO)
    an attempt at adding functionality to the "Add new audio file" button. TODO fix the action listener.

--- Soundboard.java
+++ Soundboard.java
@@ -1,11 +1,15 @@
-import javax.swing.*;    
-public class Soundboard {  
-public static void main(String[] args) {  
-    JFrame f=new JFrame("Soundboard");  
-    JButton b=new JButton("Add new audio file");  
-    b.setBounds(50,100,150,30);  
+import javax.swing.*;
+import java.awt.Event.*;    
+public class Soundboard extends JFrame {  
+public static void main(final String[] args) {  
+    final JFrame f=new JFrame("Soundboard");  
+    final JButton b=new JButton("Add new audio file");  
+    b.setBounds(50,100,150,30); 
     f.add(b);  
     f.setSize(400,400);  
     f.setLocation(300,100);  
-    f.setVisible(true);   
-}  
+    f.setVisible(true); 
+    
+    
+}
+}
