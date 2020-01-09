9d3cd67912149dabdac5d94ae6cf1e5a2111acc4
EvanJ <ejordan1305@gmail.com>
2020-01-08 23:37:27 -0600

    Added ActionListener functionallity to the button.
    Re-did the code to be able to detect when the button is pressed, and execute a task when it is. Currently only prints some text to the console. TODO add the functionality to add an audio file to the program. This will replace the output to the console and add the audio file to a variable to then be handled by the user.

--- Soundboard.java
+++ Soundboard.java
@@ -1,15 +1,39 @@
-import javax.swing.*;
-import java.awt.Event.*;    
-public class Soundboard extends JFrame {  
-public static void main(final String[] args) {  
-    final JFrame f=new JFrame("Soundboard");  
-    final JButton b=new JButton("Add new audio file");  
-    b.setBounds(50,100,150,30); 
-    f.add(b);  
-    f.setSize(400,400);  
-    f.setLocation(300,100);  
-    f.setVisible(true); 
+import javax.swing.*;                   //imports
+import java.awt.event.ActionEvent;      //imports
+import java.awt.event.ActionListener;   //imports
+ 
+public class Soundboard extends JFrame implements ActionListener {  //create class with JFrame and ActionListener
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;           // TODO find out what the heck this does
+
+    public static void main(final String[] args) {             // Main method
+    final JFrame f=new JFrame("Soundboard");                   // Create the window 
+    final JButton b=new JButton("Add new audio file");         // Create the JButton  
+    b.setBounds(50,100,150,30);                                // Create button boundaries
+    b.addActionListener(new ActionListener(){                  // Add the ActionListener to the JButton
+        
+        public void actionPerformed(final ActionEvent e) {     // Create method to do when JButton is pressed
+                System.out.println("You have added a file");   /**Task to execute when button is pressed  
+                                                                  TODO Research how to add audio files to java
+                                                                  application
+                                                                    */ 
+            }
+
+        });
+        f.add(b);                                             // Add the JButton to the JFrame
+        f.setSize(400, 400);                                  // Set the size of the JFrame
+        f.setLocation(300, 100);                              // Set the location of the JFrame
+        f.setVisible(true);                                   // Make the JFrame visable
+
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {        // TODO find out what the heck frick this does
+        
+
+    }
+
     
-    
-}
 }
